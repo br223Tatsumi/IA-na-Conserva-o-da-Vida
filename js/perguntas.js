6c3e9f145133ddb999030fa57698dbf60a0565b8
br223Tatsumi <oscarkill8@gmail.com>
2024-09-06 20:49:34 -0300

    Add files via upload
    so ta funcionando se abrir no vscode

---
+++ js/perguntas.js
@@ -0,0 +1,77 @@
+export const perguntas = [
+    {
+        enunciado: "você sempre foi curioso sobre novas tecnologias. Um dia, logo após a escola, ele descobriu um chat inovador que respondia todas as dúvidas com precisão e gerava imagens e áudios hiper-realistas. Imediatamente, ele teve uma reação:",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início, achou assustador pensar que máquinas poderiam mudar o mundo",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "No início, achou maravilhoso pensar que máquinas poderiam mudar o mundo",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Com o entusiasmo de você, sua professora de tecnologia decidiu organizar uma série de aulas sobre IA. No final de uma dessas aulas, ela pediu aos alunos que escrevessem um trabalho sobre o uso da IA na conservação da vida. você ponderou suas opções e tomou uma decisão:",
+        alternativas: [
+            {
+                texto: "Utilizar uma ferramenta de busca e IA para encontrar informações relevantes, facilitando sua compreensão do tema.",
+                afirmacao: [
+                    "mas logo percebeu que a IA podia explicar termos complicados de forma simplificada",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escrever o trabalho com base em conversas com colegas, pesquisas na internet e seu próprio conhecimento.",
+                afirmacao: [
+                    "ainda não percebeu que a IA podia explicar termos complicados de forma simplificada"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após entregar seu trabalho, você participou de uma discussão sobre o impacto da IA no futuro do trabalho. Durante a discussão, ele apresentou sua visão:",
+        alternativas: [
+            {
+                texto: "Ele defendeu a ideia de que a IA poderia criar novas oportunidades de emprego e melhorar as habilidades humanas, especialmente na área de conservação da vida.",
+                afirmacao: [
+                    "o que ajudou muito em suas pesquisas sobre conservação da vida."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Ele expressou sua preocupação com as pessoas que poderiam perder seus empregos para as máquinas, enfatizando a importância de proteger os trabalhadores, inclusive aqueles que trabalham na conservação da vida.",
+                afirmacao: [
+                    "o que ajudou muito em suas pesquisas sobre conservação da vida."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "No final da discussão, a tarefa era criar uma imagem no computador que representasse suas opiniões sobre o uso da IA na conservação da vida. você escolheu:",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA, mostrando animais selvagens em seus habitats protegidos por tecnologias de IA.",
+                afirmacao: ["Ele também notou que muitas pessoas não sabiam usar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes, promovendo a inclusão digital e a conscientização sobre a importância da tecnologia na preservação ambiental. A história de você é um exemplo de como a tecnologia pode ser percebida e utilizada de diferentes maneiras, moldando não apenas a educação, mas também o futuro do trabalho e as habilidades humanas, especialmente na importante área da conservação da vida.",
+              ],
+                proxima: 7,
+            },
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint, destacando a interação entre tecnologias de IA e a vida.",
+                afirmacao: [
+                    "Ele também notou que muitas pessoas não sabiam usar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes, promovendo a inclusão digital e a conscientização sobre a importância da tecnologia na preservação ambiental. A história de você é um exemplo de como a tecnologia pode ser percebida e utilizada de diferentes maneiras, moldando não apenas a educação, mas também o futuro do trabalho e as habilidades humanas, especialmente na importante área da conservação da vida."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+]
